22127d098f14edc5ca79124b584a40452057a3c2
Thomas Lonowski <73729800+Tomo-74@users.noreply.github.com>
2025-01-25 16:52:32 +0000

    Finished initial implementation

--- src/lab.c
+++ src/lab.c
@@ -24,33 +24,72 @@ list_t* list_init(void (*destroy_data)(void*), int (*compare_to)(const void*, co
 void list_destroy(list_t** ptr)
 {
     list_t* list = *ptr;
-    node_t* curNode = list->head->next; // start at second node (sentinel has no data to destroy)
-    while(curNode->data != NULL) {
-        list->destroy_data(curNode->data);
-        curNode = curNode->next;
+    node_t* cur = list->head->next; // Skip sentinel node
+    while(cur->data != NULL) {
+        list_remove_node(list, cur);
+        cur = list->head->next;
     }
 }
 
 list_t* list_add(list_t* list, void* data)
 {
-    node_t* head = list->head; // sentinel node
-    node_t* newNode = (node_t*)malloc(sizeof(*newNode));
-    newNode->data = data;
-    newNode->next = head;
-    newNode->prev = head->prev;
-    head->prev->next = newNode;
-    head->prev = newNode;
+    node_t* head = list->head;
+    node_t* new = (node_t*)malloc(sizeof(*new));
+
+    // Set references
+    new->data = data;
+    new->next = head;
+    new->prev = head->prev;
+    head->prev->next = new;
+    head->prev = new;
     return list;
 }
 
-// TODO
-void* list_remove_index(list_t* list, size_t index)
+// TODO Add unit tests for this functions
+void* list_remove_node(list_t* list, node_t* node)
 {
-    return 0;
+    // Update references
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+
+    // Deallocate memory
+    void* removedData = node->data;
+    list->destroy_data(node->data);
+    free(node->next); // unnecessary because next and prev are never malloc'd?
+    free(node->prev);
+    free(node);
+    return removedData;
 }
 
-// TODO
+void* list_remove_index(list_t* list, size_t index)
+{
+    // Traverse to the node to remove
+    node_t* cur = list->head->next;
+    for(size_t i=0; i<index; i++) {
+        cur = cur->next;
+    }
+    // Update references
+    cur->prev->next = cur->next;
+    cur->next->prev = cur->prev;
+    // Deallocate memory
+    void* removedData = cur->data;
+    list->destroy_data(cur->data);
+    free(cur->next);
+    free(cur->prev);
+    free(cur);
+    return removedData;
+}
+
 int list_indexof(list_t* list, void* data)
 {
-    return 0;
+    int index = 0;
+
+    // Traverse list and compare nodes
+    node_t* cur = list->head->next;
+    while(cur->data != NULL && list->compare_to(cur->data, data)) {
+        index++;
+        cur = cur->next;
+    }
+    if(cur->data == NULL) return -1;
+    return index;
 }
